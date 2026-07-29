01ab6b3f5aa2edb2a866e5aa4dcb9675171d43f0
chaten-cqst <chaten24016@cqst.in>
2026-07-29 21:47:32 +0530

    (fix): bypass email-verification in production

--- server/src/app.js
+++ server/src/app.js
@@ -26,10 +26,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/api/health", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "ResumeLab API is running",
-    });
+  res.status(200).json({
+    success: true,
+    message: "ResumeLab API is running",
+  });
 });
 
 app.use("/api/auth", authRoutes);
